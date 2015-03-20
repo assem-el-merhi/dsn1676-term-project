cec48e23959bb7115ac5318187b74bdb63ef77a2
assem-el-merhi <Assem@Assems-MacBook-Pro.local>
2015-03-20 19:24:46 -0400

    Added The rest of the javascript
    Added three buttons which would open and explain what Perception,
    shutter speed and flash when button is pressed it pops out a box which
    is a border within it provides a description of the topics. Used same
    javascript code we used in the the assignment 6 the hide/show box and
    created 3 buttons and box each with different names. Added the
    javascript.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 var navtop = document.querySelector('.nav-top');
 
+var $btnShowHide1 = $('.btn-show-hide1');
+var $box1 = $('.box1');
+
+var $btnShowHide2 = $('.btn-show-hide2');
+var $box2 = $('.box2');
+
+var $btnShowHide3 = $('.btn-show-hide3');
+var $box3 = $('.box3');
+
 document.querySelector('.nav-btn').addEventListener('click', function (e) {
     e.preventDefault();
     if (navtop.getAttribute('data-state') == 'expanded') {
@@ -10,3 +19,17 @@ document.querySelector('.nav-btn').addEventListener('click', function (e) {
     this.setAttribute('data-state', 'active');
   }
 });
+
+$btnShowHide1.on('click', function () {
+    $box1.toggleClass('js-box-expand1');
+});
+
+$btnShowHide2.on('click', function () {
+    $box2.toggleClass('js-box-expand2');
+});
+
+
+$btnShowHide3.on('click', function () {
+    $box3.toggleClass('js-box-expand3');
+});
+
