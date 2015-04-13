60c2775f815be126e66291e09b7949fb55843ed0
assem-el-merhi <Assem@Assems-MacBook-Pro.local>
2015-04-13 17:01:33 -0400

    Adding new sag
    Had problems with the older version of the sag

--- js/main.js
+++ js/main.js
@@ -45,3 +45,16 @@ $.localScroll({
   target: '.page'
 });
 
+var $flash = $('.flash');
+
+$flash.waypoint(function (direction){ // two equals signs = soft compare and three equal signs is hard compare
+  
+  if (direction == 'down')
+    $flash.addClass('js-active');
+} else {
+    $flash.removeClass('js-active');
+}
+ 
+console.log('Here');
+}, {offset: '60%'});
+
